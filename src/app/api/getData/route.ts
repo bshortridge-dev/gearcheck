20a91a19facabedc25ce915b06c361f2c8e1de0e
bshortridge-dev <bshortridge.dev@gmail.com>
2024-07-28 16:50:26 -0400

    implimented database and fetching, working on cron job

--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
+import prisma from '../../lib/prisma'
 
-const prisma = new PrismaClient()
 const classSpecs = [
   { class: 'warrior', spec: 'arms' },
   { class: 'warrior', spec: 'fury' },
